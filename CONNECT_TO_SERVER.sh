9e25f47d25aa4ec5db6b054258713e6d6ae68f80
Rafael Niebles <rnieblesealo@gmail.com>
2025-08-21 15:34:15 -0400

    chore(scripts): add echo guides

--- CONNECT_TO_SERVER.sh
+++ CONNECT_TO_SERVER.sh
@@ -3,6 +3,7 @@ SERVER_USER="root"
 SERVER_ADDR="165.232.128.10"
 
 # simple ssh into the server
+echo "SSHing to server (looking for key filename '$SSH_KEY'...)"
 ssh \
   -i "$SSH_KEY" \
   "$SERVER_USER@$SERVER_ADDR" 
