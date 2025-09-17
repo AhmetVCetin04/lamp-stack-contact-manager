505308c4664b9bf4fa49ef51bc7ee1d48b5ecd5d
Rafael Niebles <rnieblesealo@gmail.com>
2025-09-17 18:40:13 -0400

    feat(deploy): ok deploy scritp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ SSH_KEY="../.ssh/SERVER_KEY"
 SERVER_USER="root"
 SERVER_ADDR="165.232.128.10"
 
-# Glorified scp wrapper
+# Copy entire dirs "-r" 
 function deployDir () {
   # Get first arg (src dir)
   LOCAL_SRC="$1"
@@ -17,14 +17,27 @@ function deployDir () {
     "$SERVER_USER@$SERVER_ADDR:$SERVER_DEST"
 }
 
+# Copy regular files matching pattern 
+function deployPat() {
+  PATTERN="$1"
+  SERVER_DEST="$2"
+
+  echo "Copying files matching $PATTERN to $SERVER_DEST (looking for key filename '$SSH_KEY'...)"
+
+  scp \
+    -i "$SSH_KEY" \
+    "$PATTERN" \
+    "$SERVER_USER@$SERVER_ADDR:$SERVER_DEST"
+}
+
 # Match option to what we want to deploy
 opt="$1"
 echo "Received option '$opt'"
 case "$opt" in
-  # Option           # Source         # Dest
-   "api")  deployDir "../html/api/"   "/var/www/html/";;
-  "html")  deployDir "../html/*.html" "/var/www/html/";; # FIXME: This is broken
-   "css")  deployDir "../html/css/"   "/var/www/html/";;
-    "js")  deployDir "../html/js/"    "/var/www/html/";;
-       *)  deployDir "../html/"       "/var/www/";;
+  # Option         # Source       # Dest
+   api)  deployDir ../html/api/   /var/www/html/;;
+  html)  deployPat ../html/*.html /var/www/html/;; # FIXME: This is broken?
+   css)  deployDir ../html/css/   /var/www/html/;;
+    js)  deployDir ../html/js/    /var/www/html/;;
+     *)  deployDir ../html/       /var/www/;;
 esac
